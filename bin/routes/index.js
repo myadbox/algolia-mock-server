cd32c8d98b730419380ba6b0e426a60be277ac2c
Lyle Vince Dela Cuesta <hello@rairulyle.me>
2025-03-07 22:05:22 +0800

    chore: build for cli use

--- bin/routes/index.js
+++ bin/routes/index.js
@@ -1,20 +1,32 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./getObject"), exports);
-__exportStar(require("./getObjects"), exports);
-__exportStar(require("./search"), exports);
-__exportStar(require("./queries"), exports);
-__exportStar(require("./saveObjects"), exports);
-__exportStar(require("./task"), exports);
-__exportStar(require("./clear"), exports);
+'use strict'
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k
+        Object.defineProperty(o, k2, {
+          enumerable: true,
+          get: function () {
+            return m[k]
+          },
+        })
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k
+        o[k2] = m[k]
+      })
+var __exportStar =
+  (this && this.__exportStar) ||
+  function (m, exports) {
+    for (var p in m)
+      if (p !== 'default' && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p)
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+__exportStar(require('./getObject'), exports)
+__exportStar(require('./getObjects'), exports)
+__exportStar(require('./search'), exports)
+__exportStar(require('./queries'), exports)
+__exportStar(require('./saveObjects'), exports)
+__exportStar(require('./task'), exports)
+__exportStar(require('./clear'), exports)
 //# sourceMappingURL=index.js.map
